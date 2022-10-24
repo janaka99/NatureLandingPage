20eb9343770bac41b8803e81fd24daba96db19b0
Janaka Chamith <48000312+janaka99@users.noreply.github.com>
2022-10-24 22:52:41 +0530

    3 commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import Home from './Pages/Home';
 function App() {
   const navigate = useNavigate();
   return (
-
-<Routes>
-  <Route path="/" element={<Home/>} />
-</Routes>
+  <Routes>
+    <Route path="" element={<Home/>} />
+  </Routes>
   );
 }
 
